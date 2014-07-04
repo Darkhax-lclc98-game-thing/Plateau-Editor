ffec2123b35688a58fa8bb879b38a0343c377f49
lclc98 <liam.c14785@gmail.com>
2014-07-04 15:15:27 +1000

    Added Main Menu Method

--- src/main/java/plateau/Plateau.java
+++ src/main/java/plateau/Plateau.java
@@ -2,17 +2,18 @@ package plateau;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.GridLayout;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
 
 public class Plateau {
 
@@ -29,11 +30,12 @@ public class Plateau {
 		JFrame frmMain = new JFrame();
 		frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMain.setExtendedState(Frame.MAXIMIZED_BOTH);
+		frmMain.setMinimumSize(new Dimension(600, 400));
 		frmMain.setTitle("Plateau Editor");
 
 		// Creates
 		Canvas cnvs = new Canvas();
-		cnvs.setSize(400, 400);
+		cnvs.setSize(800, 400);
 
 		// TODO Remove code
 		cnvs.setBackground(new Color(200, 191, 231));
@@ -42,39 +44,77 @@ public class Plateau {
 		createMenuBar(frmMain);
 
 		// Tabs
-		createTabs(frmMain);
+		// TODO What it do?
+		// createTabs(frmMain);
 
 		frmMain.add(cnvs);
+		frmMain.setLayout(new FlowLayout());
 		frmMain.setVisible(true);
+
+		try {
+			Display.setParent(cnvs);
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		GL11.glViewport(0, 0, 800, 400);
+		
+		while (!Display.isCloseRequested()) {
+		    Display.update();
+		}
 	}
 
+	/**
+	 * Creates the main menu for the jframe
+	 * 
+	 * @param frmMain
+	 */
 	private void createMenuBar(JFrame frmMain) {
 		JMenuBar menuBar = new JMenuBar();
-		JMenu menu = new JMenu("A Menu");
-		JMenuItem menuItem = new JMenuItem("Test");
+
+		// TODO have a handler for these button presses
+		createBar("File", new JMenuItem[] {
+			new JMenuItem("New"),
+			new JMenuItem("Open"),
+			new JMenuItem(),
+			new JMenuItem("Save"),
+			new JMenuItem("Save As"),
+			new JMenuItem(),
+			new JMenuItem("Quit")}, menuBar);
+
+		createBar("Edit", new JMenuItem[] {
+			new JMenuItem("Undo"),
+			new JMenuItem("Redo"),
+			new JMenuItem(),
+			new JMenuItem("Cut"),
+			new JMenuItem("Copy"),
+			new JMenuItem("Paste") }, menuBar);
 
 		// TODO Remove code
 		menuBar.setBackground(new Color(34, 177, 76));
 
-		menu.add(menuItem);
-		menuBar.add(menu);
 		frmMain.setJMenuBar(menuBar);
 	}
 
-	private void createTabs(JFrame frmMain) {
-		JTabbedPane tabbedPane = new JTabbedPane();
-		JComponent panel1 = makeTextPanel("Panel #1");
-		tabbedPane.addTab("Test", null, panel1);
-		frmMain.add(tabbedPane);
-		tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+	/**
+	 * Adds menus to the menu bar using an array of menu items
+	 * 
+	 * @param outerBar
+	 * @param items
+	 * @param menuBar
+	 */
+	private void createBar(String outerBar, JMenuItem[] items, JMenuBar menuBar) {
+		JMenu menu = new JMenu(outerBar);
+
+		for (JMenuItem itemList : items) {
+			if (itemList.getText().equals("")) {
+				menu.addSeparator();
+			} else {
+				menu.add(itemList);
+			}
+		}
+
+		menuBar.add(menu);
 	}
 
-	protected JComponent makeTextPanel(String text) {
-		JPanel panel = new JPanel(false);
-		JLabel filler = new JLabel(text);
-		filler.setHorizontalAlignment(JLabel.CENTER);
-		panel.setLayout(new GridLayout(1, 1));
-		panel.add(filler);
-		return panel;
-	}
 }
