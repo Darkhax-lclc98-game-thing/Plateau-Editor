d96a3241ac9b8aa44bdc8cff9116281d53c9a566
lclc98 <liam.c14785@gmail.com>
2014-07-08 20:27:44 +1000

    Added map canvas to tab

--- src/main/java/plateau/Plateau.java
+++ src/main/java/plateau/Plateau.java
@@ -1,12 +1,6 @@
 package plateau;
 
-import java.awt.BorderLayout;
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -15,150 +9,160 @@ import java.awt.event.WindowListener;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class Plateau {
+    Canvas cnvs = new Canvas();
 
-	public static void main(String[] args) {
-		new Plateau().start();
-	}
+    public static void main(String[] args) {
+        new Plateau().start();
+    }
 
-	public void start() {
-		createWindow();
-	}
+    public void start() {
+        createWindow();
+    }
 
-	public void createWindow() {
-		// Sets up jframe
-		JFrame frmMain = new JFrame();
-		// sets the gui to be full screen
-		frmMain.setExtendedState(Frame.MAXIMIZED_BOTH);
-		// sets the minimum size the gui can get to
-		frmMain.setMinimumSize(new Dimension(600, 400));
-		// The title of the gui
-		frmMain.setTitle("Plateau Editor");
+    public static int getWidth() {
+        return GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+    }
 
-		// Creates canvas for map editor
-		//Canvas cnvs = new Canvas();
-		// sets size
-		//cnvs.setSize(800, 400);
+    public static int getHeight() {
+        return GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+    }
 
-		// TODO Remove code
-		//cnvs.setBackground(new Color(200, 191, 231));
+    public void createWindow() {
+        // Sets up jframe
+        JFrame frmMain = new JFrame();
+        // sets the gui to be full screen
+        frmMain.setSize(new Dimension(getWidth(), getHeight()));
+        // sets the minimum size the gui can get to
+        frmMain.setMinimumSize(new Dimension(600, 400));
+        // The title of the gui
+        frmMain.setTitle("Plateau Editor");
 
-		// Menu Bar
-		createMenuBar(frmMain);
+        // Menu Bar
+        createMenuBar(frmMain);
 
-		// Tabs
-		// TODO What it do?
-		createTabs(frmMain);
+        // Tabs
+        // TODO What it do?
+        createTabs(frmMain);
 
-		// Custom close handler
-		WindowListener exitListener = new WindowAdapter() {
+        // Custom close handler
+        WindowListener exitListener = new WindowAdapter() {
 
-			@Override
-			public void windowClosing(WindowEvent e) {
-				close();
-			}
-		};
-		frmMain.addWindowListener(exitListener);
+            @Override
+            public void windowClosing(WindowEvent e) {
+                close();
+            }
+        };
+        frmMain.addWindowListener(exitListener);
 
-		//frmMain.add(cnvs);
-		frmMain.setLayout(new FlowLayout());
-		frmMain.setVisible(true);
+        frmMain.setLayout(new FlowLayout());
+        frmMain.setVisible(true);
 
-		// sets the viewport up for the map editor
-		//new PlateauLWJGLHandler().setup(frmMain, cnvs);
-	}
+        // sets the viewport up for the map editor
+        new PlateauLWJGLHandler().setup(cnvs);
+    }
 
-	public void close() {
-		//int confirm = JOptionPane.showOptionDialog(null, "Are You Sure to Close Application?", "Exit Confirmation", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
-		//if (confirm == 0) {
-			System.exit(0);
-		//}
-	}
+    public void close() {
+        // int confirm = JOptionPane.showOptionDialog(null,
+        // "Are You Sure to Close Application?", "Exit Confirmation",
+        // JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null,
+        // null);
+        // if (confirm == 0) {
+        System.exit(0);
+        // }
+    }
 
-	/**
-	 * Creates the main menu for the jframe
-	 * 
-	 * @param frmMain
-	 */
-	private void createMenuBar(JFrame frmMain) {
-		JMenuBar menuBar = new JMenuBar();
+    /**
+     * Creates the main menu for the jframe
+     *
+     * @param frmMain
+     */
+    private void createMenuBar(JFrame frmMain) {
+        JMenuBar menuBar = new JMenuBar();
 
-		// TODO have a handler for these button presses
-		createBar("File", new JMenuItem[] {
-			new JMenuItem("New"),
-			new JMenuItem("Open"),
-			new JMenuItem(),
-			new JMenuItem("Save"),
-			new JMenuItem("Save As"),
-			new JMenuItem(),
-			new JMenuItem("Quit") }, menuBar);
+        createBar("File", new JMenuItem[]{
+                new JMenuItem("New"),
+                new JMenuItem("Open"),
+                new JMenuItem(),
+                new JMenuItem("Save"),
+                new JMenuItem("Save As"),
+                new JMenuItem(),
+                new JMenuItem("Quit")}, menuBar);
 
-		createBar("Edit", new JMenuItem[] {
-			new JMenuItem("Undo"),
-			new JMenuItem("Redo"),
-			new JMenuItem(),
-			new JMenuItem("Cut"),
-			new JMenuItem("Copy"),
-			new JMenuItem("Paste") }, menuBar);
+        createBar("Edit", new JMenuItem[]{
+                new JMenuItem("Undo"),
+                new JMenuItem("Redo"),
+                new JMenuItem(),
+                new JMenuItem("Cut"),
+                new JMenuItem("Copy"),
+                new JMenuItem("Paste")}, menuBar);
 
-		// TODO Remove code
-		menuBar.setBackground(new Color(34, 177, 76));
+        // TODO Remove code
+        menuBar.setBackground(new Color(34, 177, 76));
 
-		frmMain.setJMenuBar(menuBar);
-	}
+        frmMain.setJMenuBar(menuBar);
+    }
 
-	/**
-	 * Adds menus to the menu bar using an array of menu items
-	 * 
-	 * @param outerBar
-	 * @param items
-	 * @param menuBar
-	 */
-	private void createBar(final String menuMain, JMenuItem[] items, JMenuBar menuBar) {
-		JMenu menu = new JMenu(menuMain);
+    /**
+     * Adds menus to the menu bar using an array of menu items
+     *
+     * @param menuMain
+     * @param items
+     * @param menuBar
+     */
+    private void createBar(final String menuMain, JMenuItem[] items, JMenuBar menuBar) {
+        JMenu menu = new JMenu(menuMain);
 
-		// for each item in array add to the menu bar as a sub item
-		for (final JMenuItem itemList : items) {
-			// if the text is null, it will put a separator in instead
-			if (itemList.getText().equals("")) {
-				menu.addSeparator();
-			} else {
-				menu.add(itemList);
-				itemList.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						onClick(menuMain, itemList.getText());
-					}
-				});
-			}
-		}
+        // for each item in array add to the menu bar as a sub item
+        for (final JMenuItem itemList : items) {
+            // if the text is null, it will put a separator in instead
+            if (itemList.getText().equals("")) {
+                menu.addSeparator();
+            } else {
+                menu.add(itemList);
+                itemList.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        onClick(menuMain, itemList.getText());
+                    }
+                });
+            }
+        }
 
-		menuBar.add(menu);
-	}
+        menuBar.add(menu);
+    }
 
-	private void onClick(String menuName, String itemName) {
-		if (menuName.equalsIgnoreCase("File") && itemName.equalsIgnoreCase("Quit")) close();
-	}
+    private void onClick(String menuName, String itemName) {
+        if (menuName.equalsIgnoreCase("File") && itemName.equalsIgnoreCase("Quit")) close();
+    }
 
-	private void createTabs(JFrame frmMain) {
-		JTabbedPane tabbedPane = new JTabbedPane();
-		
-		JComponent panel1 = makeTextPanel("Panel #1");
-		tabbedPane.addTab("Test", null, panel1);
-		
-		frmMain.add(tabbedPane);
-	}
+    private void createTabs(JFrame frmMain) {
+        JTabbedPane tabbedPane = new JTabbedPane();
 
-	private JComponent makeTextPanel(String text) {
-		JPanel panel = new JPanel(false);
-		panel.setPreferredSize(new Dimension(800, 600));
-		return panel;
-	}
+        // sets size
+        cnvs.setSize(800, 400);
+
+        // TODO Remove code
+        cnvs.setBackground(new Color(200, 191, 231));
+
+        tabbedPane.addTab("Map Editor", null, makeTab(new Component[]{cnvs}));
+
+        frmMain.add(tabbedPane);
+
+
+    }
+
+    private JComponent makeTab(Component[] components) {
+        JPanel panel = new JPanel(false);
+        for (Component componet : components) {
+            panel.add(componet);
+        }
+        panel.setPreferredSize(new Dimension(getWidth() - 25, getHeight() - 100));
+        return panel;
+    }
 }
--- src/main/java/plateau/PlateauLWJGLHandler.java
+++ src/main/java/plateau/PlateauLWJGLHandler.java
@@ -7,67 +7,67 @@ import static org.lwjgl.opengl.GL11.glViewport;
 
 import java.awt.Canvas;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 
 public class PlateauLWJGLHandler {
 
-	private boolean running;
+    private boolean running;
 
-	/**
-	 * Sets up the glViewport of the map editor
-	 * 
-	 * @param frmMain
-	 * @param cnvs
-	 */
-	public void setup(JFrame frmMain, Canvas cnvs) {
-		try {
-			// sets the parent to the canvas
-			Display.setParent(cnvs);
-			// creates the display
-			Display.create();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-		}
-		// sets up view port of map editor
-		glViewport(0, 0, cnvs.getWidth(), cnvs.getHeight());
+    /**
+     * Sets up the glViewport of the map editor
+     *
+     * @param cnvs
+     */
+    public void setup(Canvas cnvs) {
+        try {
+            // sets the parent to the canvas
+            Display.setParent(cnvs);
+            // creates the display
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+        // sets up view port of map editor
+        glViewport(0, 0, cnvs.getWidth(), cnvs.getHeight());
 
-		// sets the viewport to running
-		running = true;
+        // sets the viewport to running
+        running = true;
 
-		loop();
-	}
+        loop();
+    }
 
-	/**
-	 * Loops the display
-	 */
-	private void loop() {
-		while (running) {
-			// clears the render buffer
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    /**
+     * Loops the display
+     */
+    private void loop() {
+        while (running) {
+            // clears the render buffer
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			// updates the viewport and surrounding gui components
-			Display.update();
+            // updates the viewport and surrounding gui components
+            Display.update();
 
-			// checks if a close request is sent
-			isRequestShutdown();
-		}
-	}
+            // checks if a close request is sent
+            isRequestShutdown();
+        }
+    }
 
-	/**
-	 * Checks for close request
-	 */
-	private void isRequestShutdown() {
-		// if close request is sent
-		if (Display.isCloseRequested()) {
-			// stop running loop
-			this.running = false;
+    /**
+     * Checks for close request
+     */
+    private void isRequestShutdown() {
+        // if close request is sent
+        if (Display.isCloseRequested()) {
+            // stop running loop
+            this.running = false;
 
-			// destroy the lwjgl display
-			Display.destroy();
-		}
-	}
+            // destroy the lwjgl display
+            Display.destroy();
+        }
+    }
+
 
 }
